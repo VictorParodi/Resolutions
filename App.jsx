728541e1fda7f81cf2b11911fc70d7e3ba97e062
VictorParodi <victor.parodi.montero@gmail.com>
2018-08-20 15:44:25 -0500

    app1 commit 3: meteortoys installed. The first collection created

--- App.jsx
+++ App.jsx
@@ -1,10 +1,36 @@
 import React, {Component} from 'react';
-import {render} from 'react-dom';
+import {Mongo} from 'meteor/mongo';
 
+const Resols = new Mongo.Collection('resols');
 class App extends Component {
+    addResolution(event) {
+        event.preventDefault();
+        let text     = this.refs.resolution.value.trim(),
+            newResol = {
+                text,
+                complete: false,
+                createdAt: new Date()
+            }
+            
+        Resols.insert(newResol);
+
+        this.refs.resolution.value = '';
+    }
+
     render() {
         return (
-            <p>App.jsx component: app1</p>
+            <div>
+                <h1>Resolutions app1</h1>
+                <form className="new-resolution" onSubmit={this.addResolution.bind(this)}>
+                    <input
+                        type="text"
+                        name="resolution"
+                        ref="resolution"
+                        placeholder="Enter a new resolution"
+                    />
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
         );
     }
 }
