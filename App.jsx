f99cddd13f7f5b0410be9e120cae2fbf4c7f5e94
VictorParodi <victor.parodi.montero@gmail.com>
2018-08-20 01:40:02 -0500

    app3 commit 2
    Inserting documents into database. Install meteortoys

--- App.jsx
+++ App.jsx
@@ -1,11 +1,37 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import {Meteor} from 'meteor/meteor';
+import {Mongo} from 'meteor/mongo';
+
+const Resolutions = new Mongo.Collection("resolutions");
 
 class App extends Component {
+    addResolution(event) {
+        event.preventDefault();
+        let text = this.refs.resolution.value.trim();
+
+        Resolutions.insert({
+            text,
+            complete: false,
+            createdAt: new Date()
+        });
+
+        this.refs.resolution.value = '';
+    }
+
     render() {
         return (
-            <p>App.jsx Component</p>
+            <div>
+                <h1>Resolutions app3</h1>
+                <form className="new-resolution" onSubmit={this.addResolution.bind(this)}>
+                    <input 
+                        type="text"
+                        ref="resolution"
+                        name="resolution"
+                        placeholder="New resolution"
+                    />
+                </form>
+            </div>
         );
     }
 }
