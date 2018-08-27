0aba2e9cb80af2d8b0d6e9a99b641fe0c4b4d5c9
VictorParodi <victor.parodi.montero@gmail.com>
2018-08-26 21:45:08 -0500

    app3 commit 9
    Updating and removing documents

--- client/resolutions/ResolutionSingle.jsx
+++ client/resolutions/ResolutionSingle.jsx
@@ -1,12 +1,35 @@
 import React, {Component} from 'react';
+import {Meteor} from 'meteor/meteor';
 
 class ResolutionSingle extends Component {
+    toggleChecked() {
+        let {_id, complete} = this.props.resolution;
+        Meteor.call('toggleResolution', _id, complete);
+    }
+
+    deleteResolution() {
+        let {_id} = this.props.resolution;
+        Meteor.call('deleteResolution', _id);
+    }
+
     render() {
         let {text, complete} = this.props.resolution;
 
         return (
             <li>
+                <input
+                    type="checkbox"
+                    readOnly={true}
+                    checked={complete}
+                    onClick={this.toggleChecked.bind(this)}
+                />
                 {text} - {complete.toString()}
+                <button
+                    className="btn-cancel"
+                    onClick={this.deleteResolution.bind(this)}
+                >
+                    x
+                </button>
             </li>
         );
     }
