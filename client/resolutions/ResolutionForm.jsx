daa83503bef6a41baf306b248ca081df9a721e1c
VictorParodi <victor.parodi.montero@gmail.com>
2018-09-01 19:12:52 -0500

    app3 commit 13
    Adding nice notifications with Bert

--- client/resolutions/ResolutionForm.jsx
+++ client/resolutions/ResolutionForm.jsx
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
 import {Meteor} from 'meteor/meteor';
+import {Bert} from 'meteor/themeteorchef:bert';
 
 class ResolutionForm extends Component {
     addResolution(event) {
         event.preventDefault();
         let text = this.refs.resolution.value.trim();
 
-        Meteor.call('addResolution', text, () => {
-            this.refs.resolution.value = '';
-        });
+        if (text) {
+            Meteor.call('addResolution', text, () => {
+                this.refs.resolution.value = '';
+            });
+        } else {
+            Bert.alert('Need a text for resolution', 'danger', 'growl-top-right', 'fa-frown-o');
+        }
     }
 
     render() {
